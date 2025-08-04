f5135fb9c23ce0a019f1ed5de5b8a3224a64466e
jjokah <johnjohnsonokah@gmail.com>
2025-08-04 20:28:54 +0100

    Build a greeting bot

---
+++ full-stack/javascript/greeting_bot.js
@@ -0,0 +1,33 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+
+let bot;
+let botLocation;
+
+bot = "teacherBot";
+botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!");

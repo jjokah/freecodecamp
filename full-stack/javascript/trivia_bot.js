0050110f42865d6f7876badba76249e1e7ac2f5d
jjokah <johnjohnsonokah@gmail.com>
2025-08-05 07:52:10 +0100

    Build a JavaScript Trivia Bot

---
+++ full-stack/javascript/trivia_bot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+const botName = "jjokahBot";
+const botLocation = "Naiversity";
+const favoriteLanguage = "Unconscious Lang";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact;
+codingFact = favoriteLanguage + " is actually a subconscious programming language.";
+console.log(codingFact);
+codingFact = "Everyone uses the " + favoriteLanguage + ".";
+console.log(codingFact);
+codingFact = "Dream is the " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");

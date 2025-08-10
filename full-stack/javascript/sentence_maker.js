4be4b3f365a0dbb739b25d332f377a6ef62c5fe0
jjokah <johnjohnsonokah@gmail.com>
2025-08-10 15:04:56 +0100

    Build a Sentence Maker

---
+++ full-stack/javascript/sentence_maker.js
@@ -0,0 +1,23 @@
+let firstStory;
+
+let adjective = "pretty";
+let noun = "lady";
+let verb = "touched";
+let place = "city";
+let adjective2 = "magical";
+let noun2 = "noodles";
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " " + "lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory)
+
+adjective = "naughty";
+noun = "boy";
+verb = "hit";
+place = "Nigeria";
+adjective2 = "cool";
+noun2 = "yam";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " " + "lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("Second story: " + secondStory)
